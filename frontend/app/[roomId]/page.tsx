d1009ed9992f52562f8f491bd4f21f27394ea74e
RidhikaJoshi <ridhikasmrh@gmail.com>
2025-01-21 15:34:22 +0530

    Refactor RoomPage component to use useParams for roomId retrieval and add loading state

--- frontend/app/[roomId]/page.tsx
+++ frontend/app/[roomId]/page.tsx
@@ -1,8 +1,16 @@
+'use client'
 import React from 'react'
 import CollaborativePage from '../../components/collaborative-page'
+import { useParams } from 'next/navigation'
 
-const RoomPage =  ({ params }: { params: { roomId: string } }) => {
-  return <CollaborativePage roomId={params.roomId} />
+const RoomPage: React.FC = () => {
+  const { roomId } = useParams()
+
+  if (!roomId || typeof roomId !== 'string') {
+    return <div>Loading...</div>
+  }
+
+  return <CollaborativePage roomId={roomId} />
 }
 
 export default RoomPage
