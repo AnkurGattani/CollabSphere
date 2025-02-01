7fa95f1ac63a7cc0a0d278622ce4d2863f716663
Ankur Gattani <ankurgattani02@gmail.com>
2025-02-02 02:10:03 +0530

    Add Liveblocks authentication API and update environment configuration

---
+++ frontend/app/api/liveblocks-auth/route.ts
@@ -0,0 +1,28 @@
+import { Liveblocks } from '@liveblocks/node';
+import { useAuthStore } from '../../../store/authStore';
+
+const liveblocks = new Liveblocks({
+    secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+})
+
+export async function POST(req: Request) {
+        const { user, room } = await req.json();
+
+    // generate color based on user's name
+    const name = user.firstName + " " + user.lastName;
+    const nameToNumber = name.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    const color = `hsl(${nameToNumber % 360}, 80%, 60%)`;
+
+
+    const session = liveblocks.prepareSession(user.email, {
+        userInfo: {
+            name,
+            color,
+        }
+    });
+
+    session.allow(room, session.FULL_ACCESS);
+    const { body, status } = await session.authorize();
+
+    return new Response(body, { status });
+}
