ee2731b4ee23d385c26293e0287f68766ad26da6
RIDHIKA JOSHI <92508014+RidhikaJoshi@users.noreply.github.com>
2025-06-07 20:48:15 +0530

    Update user.routes.ts

--- Backend/src/routes/user.routes.ts
+++ Backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,getUser } from "../controllers/user.controller";
+import { registerUser,loginUser,logoutUser,getUser,getUserName } from "../controllers/user.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router(); // this method is used to create a new router object
@@ -8,5 +8,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,logoutUser);
 router.route("/getUser").get(getUser);
+router.route("/getUserName").get(getUserName);
 
 export default router;
